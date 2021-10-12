10419c31d46097f25e48418685d63052e1fdf066
Vlasovvic <85340854+Vlasovvic@users.noreply.github.com>
2021-10-12 21:26:38 +0300

    Add files via upload

---
+++ NTT.py
@@ -0,0 +1,8 @@
+# Write Python 3 code in this online editor and run it.
+import random
+
+for i in range(1,100):
+    a=random.randint(1,9)
+    b=random.randint(1,9)
+    print(a," X ", b)
+    i=i+1
